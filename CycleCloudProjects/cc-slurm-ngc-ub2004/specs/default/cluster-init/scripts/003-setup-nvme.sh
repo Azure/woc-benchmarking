6927fae5d3e72e7605d9671d0dcd98e0556eb1df
Jon Shelley <JonShelley@users.noreply.github.com>
2022-10-21 08:48:06 -0600

    Update 003-setup-nvme.sh

--- CycleCloudProjects/cc-slurm-ngc-ub2004/specs/default/cluster-init/scripts/003-setup-nvme.sh
+++ CycleCloudProjects/cc-slurm-ngc-ub2004/specs/default/cluster-init/scripts/003-setup-nvme.sh
@@ -9,12 +9,27 @@ if [ "$NVME_DISKS" == "0" ]
 then
     exit 0
 else
-    mkdir -p /mnt/resource_nvme
+    MNT_POINT=/mnt/resource_nvme
+    mkdir -p $MNT_POINT
     # Needed incase something did not unmount as expected. This will delete any data that may be left behind
+    sudo umount $MNT_POINT
     mdadm  --stop /dev/md*
     mdadm --create /dev/md128 -f --run --level 0 --raid-devices $NVME_DISKS $NVME_DISKS_NAME
+
+    # Setup the disk
     mkfs.xfs -f /dev/md128
-    mount /dev/md128 /mnt/resource_nvme
+
+    # Mount the disk
+    blkid
+    string=`blkid | grep md128`
+    arrIN=(${string// / })
+    name_str=${arrIN[1]}
+    arrIN2=(${name_str//=/ })
+    DISK_NAME=${arrIN2[1]}
+
+    echo  "UUID=${DISK_NAME} $MNT_POINT xfs defaults 0 0" >> /etc/fstab
+    systemctl daemon-reload
+    systemctl reset-failed
 fi
 
-chmod 1777 /mnt/resource_nvme
+chmod 1777 $MNT_POINT
