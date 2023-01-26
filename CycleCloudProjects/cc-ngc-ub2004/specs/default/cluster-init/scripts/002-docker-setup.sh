574e587073dc85822ed13c8fcb300e5673ee9d05
Jon Shelley <JonShelley@users.noreply.github.com>
2023-01-26 15:31:08 -0700

    Update 002-docker-setup.sh
    Removed "sudo" since it is not needed and updated the docker run line

--- CycleCloudProjects/cc-ngc-ub2004/specs/default/cluster-init/scripts/002-docker-setup.sh
+++ CycleCloudProjects/cc-ngc-ub2004/specs/default/cluster-init/scripts/002-docker-setup.sh
@@ -10,11 +10,8 @@ set -ex
 #### Install Docker                                                       
 echo "\n---------------- Install Docker ----------------"
 cd /mnt/resource
-sudo apt update
+apt update
 curl https://get.docker.com | sh && sudo systemctl --now enable docker
-#sudo dpkg -P moby-engine
-#sudo dpkg -P moby-cli
-#sudo apt install -y docker.io
 
 #### Install NV-DOCKER                                                       
 echo "\n---------------- Install NV-Docker ----------------"
@@ -23,35 +20,36 @@ cd /mnt/resource
 docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
 
 set +e
-sudo apt-get purge -y nvidia-docker
+apt-get purge -y nvidia-docker
 set -e
 
 # Add the package repositories
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
-  sudo apt-key add -
+  apt-key add -
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
-  sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update
-sudo apt-get install -y nvidia-docker2=2.6.0-1
-sudo pkill -SIGHUP dockerd
+  tee /etc/apt/sources.list.d/nvidia-docker.list
+apt-get update
+apt-get install -y nvidia-docker2=2.6.0-1
+pkill -SIGHUP dockerd
 
 # Update the docker config file
-sudo systemctl stop docker
-sudo sh -c "echo '{  \"data-root\": \"/mnt/resource/docker\", \"bip\": \"152.26.0.1/16\", \"runtimes\": { \"nvidia\": { \"path\": \"/usr/bin/nvidia-container-runtime\", \"runtimeArgs\": [] } } }' > /etc/docker/daemon.json"
-sudo systemctl restart docker
+systemctl stop docker
+sh -c "echo '{  \"data-root\": \"/mnt/resource/docker\", \"bip\": \"152.26.0.1/16\", \"runtimes\": { \"nvidia\": { \"path\": \"/usr/bin/nvidia-container-runtime\", \"runtimeArgs\": [] } } }' > /etc/docker/daemon.json"
+systemctl restart docker
 
 set +e
-sudo docker run --runtime=nvidia --rm nvidia/cuda:11.0-base nvidia-smi
+# docker run --runtime=nvidia --rm nvidia/cuda:11.0-base nvidia-smi
+docker run --runtime=nvidia --rm nvcr.io/nvidia/cuda:11.0.3-base-ubuntu18.04 nvidia-smi
 set -e
 
 ### Install NVtop
 echo "\n---------------- Install NVtop ----------------"
-sudo apt install -y cmake libncurses5-dev libncursesw5-dev git
+apt install -y cmake libncurses5-dev libncursesw5-dev git
 cd /tmp
 cp ${CYCLECLOUD_SPEC_PATH}/files/nvtop-2.0.3.tar.gz .
 tar xzf nvtop-2.0.3.tar.gz
 cd nvtop-2.0.3
 cmake .
 make
-sudo make install
+make install
